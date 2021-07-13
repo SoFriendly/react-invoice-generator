71edf3c8972ea624590a9ec42de6457225379373
Justin Mitchell <justin@jmitch.com>
2021-07-13 03:26:40 +0000

    Updated homepage & copy

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="app">
       <h1 className="center fs-30">SoFriendly Invoice Generator</h1>
-      <p className="left fs-10">Quickly generate invoices from us, to you, at SoFriendly. We often don't have time to generate invoices but we know how important it is for you to have copies for taxes and compliance. So, go ahead, generate your own and download the PDF.</p>
+      <p className="left fs-10">Quickly generate invoices from us at SoFriendly to you. We often don't have time to generate invoices but we know how important it is for you to have copies for taxes and compliance. So, go ahead, generate your own and download the PDF.</p>
       <InvoicePage />
     </div>
   )
